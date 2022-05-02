759f83c036da25d762bc30f2e6ce05900a8fe6d2
Rikvandenheede <rik.vandenheede0@hotmail.com>
2022-05-02 22:56:28 +0200

    3de keer goede keer xd

---
+++ src/components/atoms/Weirdchamp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="weirdchamp">
+    <h2 class="weirdchamp__header">
+      {{ person.name }} is een {{ person.description }}
+    </h2>
+    <p class="weirdchamp__content">
+      Waarheid Waarheid Waarheid Waarheid Waarheid Waarheid
+    </p>
+    <img class="weirdchamp__img" src="../../../public/for.jpeg" alt="meme" />
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "WeirdChamp",
+  props: {
+    person: Object,
+  },
+});
+</script>
