672c78dbeff9deabda882ee790352903c0195400
Rikvandenheede <rik.vandenheede0@hotmail.com>
2022-05-03 10:22:05 +0200

    layout

---
+++ src/components/app/atoms/Layout.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main">{{ title.title }}</div>
+</template>
+
+<script>
+/* eslint-disable */
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Layout",
+  props: {
+    title: Object,
+  },
+});
+</script>
--- src/views/app/HomeView.vue
+++ src/views/app/HomeView.vue
@@ -1,20 +1,22 @@
 <template>
   <main>
-    <WeirdChamp :person="{ name: 'Kai', description: 'Knuppel' }" />
+    <Layout :title="{ title: 'Test' }"> </Layout>
     <Navigation />
   </main>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import WeirdChamp from "@/components/app/atoms/Weirdchamp.vue";
+// import WeirdChamp from "@/components/app/atoms/Weirdchamp.vue";
 import Navigation from "@/components/app/molecules/Navigation.vue";
+import Layout from "@/components/app/atoms/Layout.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
-    WeirdChamp,
+    // WeirdChamp,
     Navigation,
+    Layout,
   },
 });
 </script>
