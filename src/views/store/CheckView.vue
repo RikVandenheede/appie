a9b108cd6200871f9ae5c1a3edcf5c1026b4c23f
Tristan Voorjans <tristan.voorjans@criptext.com>
2022-05-10 11:02:15 +0200

    success

--- src/views/store/CheckView.vue
+++ src/views/store/CheckView.vue
@@ -1,18 +1,13 @@
 <template>
 	<main>
-		<h1>Dit is de check view.</h1>
-		<h1>Product werd ingescand.</h1>
-		<h1>Alles werd gescand, <br>
-		u kunt naar de kassa.</h1>
-		
-        
-        <Check />
+		<h1 class="leading">Product werd ingescand.</h1>
+		<Check />
 	</main>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
-import Check from '@/components/store/Check.vue';
+import { defineComponent } from 'vue'
+import Check from '@/components/store/Check.vue'
 
 // import WeirdChamp from "@/components/store/atoms/Weirdchamp.vue";
 // import Navigation from "@/components/store/molecules/Navigation.vue";
