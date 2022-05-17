e0ceec50661772b786e76844d43e3273441d3245
Rikvandenheede <rik.vandenheede0@hotmail.com>
2022-05-17 10:53:11 +0200

    merge both

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,11 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router';
-// import HomeView from '../views/store/HomeView.vue';
-import scan from '../views/store/ScanView.vue';
-import Loading from '../views/store/LoadingView.vue';
-import NavWalk from '../views/store/NavWalkView.vue';
-import Check from '../views/store/CheckView.vue';
-import Qr from '../views/store/QrView.vue';
-
-const routes = [
-    // {
-    //     path: '/',
-    //     name: 'home',
-    //     component: HomeView
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'about',
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ '../views/store/AboutView.vue')
-    // },
-    {
-        path: '/',
-        name: 'scan',
-        component: scan
-    },
-    {
-        path: '/loading',
-        name: 'loading',
-        component: Loading
-    },
-    {
-        path: '/navwalk',
-        name: 'navwalk',
-        component: NavWalk
-    },
-    {
-        path: '/check',
-        name: 'check',
-        component: Check
-    },
-    {
-        path: '/qr',
-        name: 'qr',
-        component: Qr
-    },
-
-];
-
-const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
-=======
 import { createRouter, createWebHistory } from "vue-router";
+// import HomeView from '../views/store/HomeView.vue';
+import scan from "../views/store/ScanView.vue";
+import Loading from "../views/store/LoadingView.vue";
+import NavWalk from "../views/store/NavWalkView.vue";
+import Check from "../views/store/CheckView.vue";
+import Qr from "../views/store/QrView.vue";
+
 import HomeView from "../views/app/HomeView.vue";
 import Winkelwagentje from "../views/app/WinkelwagenView.vue";
 import ItemDetailView from "../views/app/ItemDetailView.vue";
@@ -63,17 +15,33 @@ import CategoryPage from "../views/app/CategorieView.vue";
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "scan",
+    component: scan,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/app/AboutView.vue"),
+    path: "/loading",
+    name: "loading",
+    component: Loading,
+  },
+  {
+    path: "/navwalk",
+    name: "navwalk",
+    component: NavWalk,
+  },
+  {
+    path: "/check",
+    name: "check",
+    component: Check,
+  },
+  {
+    path: "/qr",
+    name: "qr",
+    component: Qr,
+  },
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
   },
   {
     path: "/winkelwagentje",
@@ -82,7 +50,7 @@ const routes = [
   },
   {
     path: "/item/:id",
-    name: 'ItemDetailView',
+    name: "ItemDetailView",
     props: true,
     component: ItemDetailView,
   },
@@ -101,7 +69,6 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
->>>>>>> a73825a2d61f6ab65deb64954d24b537e3a1627a
 });
 
 export default router;
