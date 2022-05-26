b28a99477b893a1fd0f74d874f4312a0dc72bec7
Tristan Voorjans <tristan.voorjans@criptext.com>
2022-05-26 08:32:36 +0200

    update ending scene

--- src/views/store/QrCheckoutView.vue
+++ src/views/store/QrCheckoutView.vue
@@ -6,10 +6,13 @@
 				<br />
 				om af te rekenen
 			</h1>
-			<QrCheckout />
+			<QrCheckout @click="endingOn()" />
 		</div>
 		<div class="store-ending" v-show="ending">
-			<img src="images/images/ending.png" alt="ending scene bloody knife">
+			<img
+				src="images/images/ending.png"
+				alt="ending scene bloody knife"
+			/>
 		</div>
 	</div>
 </template>
@@ -23,12 +26,17 @@ import QrCheckout from '@/components/store/QrCheckout.vue';
 export default defineComponent({
 	name: 'QrView',
 	components: { QrCheckout },
-
 	data() {
 		return {
-			checkout: false,
-			ending: true,
+			checkout: true,
+			ending: false,
 		};
+	},
+	methods: {
+		endingOn() {
+			this.checkout = false;
+			this.ending = true;
+		},
 	},
 });
 </script>
